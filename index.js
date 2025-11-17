173d48ef7dfe925e5be68394d00c2b00ce843af6
binuka2009 <binukaudayanga3@gmail.com>
2025-11-18 01:19:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ console.log("Session downloaded ✅")
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 9090;
+const port = process.env.PORT || 8000;
   
   //=============================================
   
