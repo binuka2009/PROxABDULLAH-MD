5dc952cb586f7bf76e913ea08df1ec3f482ebf2a
binuka2009 <binukaudayanga3@gmail.com>
2025-11-18 09:10:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,16 +49,22 @@ const {
   const ownerNumber = ['923237045919']
 
   //===================SESSION-AUTH============================
-if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID
-const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
-filer.download((err, data) => {
-if(err) throw err
-fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
-console.log("Session downloaded ✅")
-})})}
-
+if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
+    if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+    
+    const sessdata = config.SESSION_ID.replace("ARSLAN-MD~", '');
+    try {
+        // Decode base64 string
+        const decodedData = Buffer.from(sessdata, 'base64').toString('utf-8');
+        
+        // Write decoded data to creds.json
+        fs.writeFileSync(__dirname + '/sessions/creds.json', decodedData);
+        console.log("Session loaded ✅");
+    } catch (err) {
+        console.error("Error decoding session data:", err);
+        throw err;
+    }
+}
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
